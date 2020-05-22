48766eb9b86aacca5a7093a232c0a31668a6acd9
Alex Macniven <macniven.ap@gmail.com>
2020-05-22 11:10:57 +0100

    Adds Tetromino Arrays

--- app.js
+++ app.js
@@ -12,5 +12,55 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 10
     const scoreDisplay = document.querySelector('#score')
     const startButton = document.querySelector('#start-button')
+    // Map out the coordinates of the L shaped tetrominoes
+    // Check out https://youtu.be/rAUn1Lom6dw?t=2160
+    // Creating a nested array where each inner array element is a collection of coordinates
+    // Each coordinate describes a coloured block of the tetronome
+    // As the grid has a width of 10, each new row has a coordinate of width
+    /* |01|02|03|
+       |10|11|12|
+       |20|21|22|
+    */
+   // A tetromino is drawn like;
+   /*  |  |xx|  |
+      |  |xx|  |
+     |xx|xx|  |
+    */
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2, width*2+1],
+        [width, width*2, width*2+1, width*2+2]
+    ]
 
+    const zTetromino = [
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1]
+    ]
+
+    const tTetromino = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1],
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+
+    ]
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 })
